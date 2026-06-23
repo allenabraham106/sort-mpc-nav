8bef99256a6a24fcd9ba77c5794b003e474003e3
Allen Abraham <allenabraham106@gmail.com>
2026-06-22 20:55:32 -0600

    main function and code fixes

--- sort_mpc_nav/sort_tracker.py
+++ sort_mpc_nav/sort_tracker.py
@@ -9,10 +9,10 @@ class SortTracker(Node):
     def __init__(self):
         super().__init__('sort_tracker')
         self.num_pedestrians = 3
-        self.callback_group = ReentrantCallbackGroup
+        self.callback_group = ReentrantCallbackGroup()
 
         # one kalman filter per pedestrian
-        self.filters = [self.init_kalman_filter for _ in range(self.num_pedestrians)]
+        self.filters = [self.init_kalman_filter() for _ in range(self.num_pedestrians)]
 
         # track if the pedestrain has received its first measurment
         self.initialized = [False] * self.num_pedestrians
@@ -27,12 +27,13 @@ class SortTracker(Node):
                 10,
                 callback_group = self.callback_group
             )
+            self.subscribers.append(sub)
         
         # publishers (one per pedestrian)
         # publishing pose and velocity
         self.pose_publishers = []
         self.vel_publishers = []
-        for u in range(self.nume_pedestrians):
+        for u in range(self.num_pedestrians):
             pose_pub = self.create_publisher(
                 Point,
                 f'/tracked_{u}/pose',
@@ -55,7 +56,7 @@ class SortTracker(Node):
         # Transition matrix F
         kf.F = np.array([
             [1, 0, dt, 0],
-            [0, 1, 0, 1],
+            [0, 1, 0, dt],
             [0, 0, 1, 0],
             [0, 0, 0, 1]
         ])
@@ -92,7 +93,7 @@ class SortTracker(Node):
         kf.predict()
 
         # Update with actual measurements (step 2 in pose_callback)
-        measurment = np.array([
+        measurement = np.array([
             [msg.x],
             [msg.y]
         ])
@@ -102,7 +103,7 @@ class SortTracker(Node):
         px = kf.x[0, 0]
         py = kf.x[1, 0]
         vx = kf.x[2, 0]
-        vy = kf.y[3, 0]
+        vy = kf.x[3, 0]
 
         # publish estimated pose
         pose_msg = Point()
@@ -117,3 +118,17 @@ class SortTracker(Node):
         vel_msg.y = vy
         vel_msg.z = 0.0
         self.vel_publishers[idx].publish(vel_msg)
+
+def main(args = None):
+    rclpy.init(args = args)
+    node = SortTracker()
+    try: 
+        rclpy.spin(node)
+    except KeyboardInterupt: 
+        pass
+    finally: 
+        node.destroy_node()
+        rclpy.shutdown()
+    
+if __name__ == '__main__':
+    main()
